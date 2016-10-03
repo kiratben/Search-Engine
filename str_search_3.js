12b3f4e0e947864658f533b80df0d1d5693e3e53
tben1 <tarikbenabbou@gmail.com>
2016-10-03 01:48:06 +0100

    update str_search_3 (comments)

--- str_search_3.js
+++ str_search_3.js
@@ -1,24 +1,24 @@
 var subStrSearch = function(strs, txt){
     
     var res = {};
-
+	//loop on each element of array
     strs.forEach(function(e){
-
+		//define res key
         res[e] = 0;
-
+		//iterate over text length
         for(var i = 0; i < txt.length;){
 
             var pos = txt.indexOf(e, i)
             
             if(pos > -1){
-
+			//incr res at key of element
                 res[e]++;
 
                 i = pos+1;
                 
             }
             else{
-
+			//incr i to continue search
                 i++;
             }
    
