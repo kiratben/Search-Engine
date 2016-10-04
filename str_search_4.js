c1fcd25734b8902a460678ff11cf61b552d8e6de
tben1 <tarikbenabbou@gmail.com>
2016-10-05 00:07:11 +0100

    update str_search_4

--- str_search_4.js
+++ str_search_4.js
@@ -2,11 +2,21 @@ exports.subStrSearch = function (strs, txt) {
     var res = {},
         i = strs.length,
         e = '', 
-         pos = 0, 
-         cmp = 0;
+        pos = 0, 
+        cmp = 0;
 
 while (i--) {
-       //r()
+        e = strs[i]; cmp = 0;
+        if (e.length === 0) { 
+            pos = -1;
+        } else {
+            pos = txt.indexOf(e);
+        }
+        while (pos !== -1) {
+            cmp++;
+            pos = txt.indexOf(e, pos + e.length); 
+        }
+        res[e] = cmp;
     }
 
     return res;
